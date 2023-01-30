5690531ffb19739565992dd9337deac2ad50f4e1
Okan <okso2004@gmail.com>
2023-01-30 19:40:41 +0100

    Created structure of output

--- visual.py
+++ visual.py
@@ -1,4 +1,6 @@
 from tkinter import *
+# from functions import *
+
 window = Tk()
 
 # Entry field for function (e.g. f(x) = x^2)
@@ -7,6 +9,9 @@ entry = Entry(window).grid(row=0, column=1)
 
 # Button for generating solutions
 generate = Button(window, text="Generate solutions", command={}).grid(row=1, column=1)
+
+# Text field for showing content
+output = Label(window, text="solutions").grid(row=2, column=0)
 
 window.title('Visual Curve Discussion')
 window.geometry("1000x1000")
